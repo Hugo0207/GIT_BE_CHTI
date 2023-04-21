15446afcda9b98b6908042e2364757201bab0e7f
Hugo Girard <hgirard002@gmail.com>
2023-04-21 15:43:43 +0200

    DFT_Debut

--- ASM/PjtKEIL_StepSon/Src/GestionSon.s
+++ ASM/PjtKEIL_StepSon/Src/GestionSon.s
@@ -1,10 +1,12 @@
 	PRESERVE8
 	THUMB   
 	export callbackson
+	export startson
 	import Son
 	import LongueurSon
 	export index
-		
+	import PWM_Set_Value_TIM3_Ch3
+	include DriverJeuLaser.inc
 
 ; ====================== zone de réservation de données,  ======================================
 ;Section RAM (read only) :
@@ -34,9 +36,8 @@ index dcw 0
 ;Section ROM code (read only) :		
 	area    moncode,code,readonly
 ; écrire le code ici		
-
 callbackson PROC
-	
+	push {lr,r4,r5,r6,r7}
 	ldr r0,=index
 	ldr r1,=LongueurSon
 	ldr r2,[r0]
@@ -55,12 +56,27 @@ callbackson PROC
 	
 	ldr r7,=sortieson
 	strh r5,[r7]
-	
+	push {r0,r1,r2,r3}
+	mov r0, r5
+	bl PWM_Set_Value_TIM3_Ch3
+	pop {r0,r1,r2,r3}
 	add r2,#1
+
 	strh r2,[r0]
 	
 Exit
+	pop {lr,r4,r5,r6,r7}
 	bx lr
 	ENDP
 		
+startson PROC
+	push{lr,r4,r5,r6,r7}
+	ldr r0,=index
+	ldr r1,[r0]
+	mov r1,#0
+	strh r1,[r0]
+	pop{lr,r4,r5,r6,r7}	
+	bx lr
+	ENDP
+	
 	END	
--- ASM/PjtKEIL_StepSon/Src/principal.c
+++ ASM/PjtKEIL_StepSon/Src/principal.c
@@ -3,6 +3,7 @@
 #include "DriverJeuLaser.h"
 
 void callbackson(void);
+void startson(void);
 
 int main(void)
 {
@@ -14,15 +15,18 @@ int main(void)
 // Après exécution : le coeur CPU est clocké à 72MHz ainsi que tous les timers
 CLOCK_Configure();
 
-	
+
 Timer_1234_Init_ff(TIM4, 91*72);//en microseconde
 Active_IT_Debordement_Timer(TIM4, 2, callbackson);
-
+Timer_1234_Init_ff(TIM2, 72000000);//en microseconde
+Active_IT_Debordement_Timer(TIM2, 1, startson);
 	
 	
 
 //============================================================================	
 	
+PWM_Init_ff(TIM3, 3, 720);
+GPIO_Configure(GPIOB, 0, OUTPUT, ALT_PPULL);
 	
 while	(1)
 	{
